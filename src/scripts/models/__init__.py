ef7b4225412e94def300985ef6fe9d22751e2be0
viggo-gascou <94069687+viggo-gascou@users.noreply.github.com>
2022-11-29 15:12:03 +0100

    fix path

--- src/scripts/models/__init__.py
+++ src/scripts/models/__init__.py
@@ -1,5 +1,6 @@
 from .neural_net import NeuralNetworkClassifier
 from .neural_net import DenseLayer
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 __all__ = [
         'NeuralNetworkClassifier',
