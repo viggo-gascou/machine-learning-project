92ef7dd9012fab4b7c3dc1e8f841296242f552f3
viggo-gascou <94069687+viggo-gascou@users.noreply.github.com>
2022-12-06 16:04:46 +0100

    Data loader update
    - Added pca and scaled dataset
    - Updated data loader to include pca and scaled versions of dataset

--- src/mlproject/helpers/_data_loader.py
+++ src/mlproject/helpers/_data_loader.py
@@ -4,33 +4,50 @@ from sklearn.preprocessing import StandardScaler
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '../..'))
 
-def data_loader(raw=True, scaled=False):
+def data_loader(raw=True, scaled=False, pca=False):
     """Loads the fashion_mnist training and test data from the data directory.
     The class returns four numpy arrays containing the training and test data
     respectively. 
-    If specified it can also return the standard scaled version of the data.
+    If specified it can also return the standard scaled version of the data or 
+    the first 10 principal components of the data.
 
-    Training data:                
-        `X_train`: (10000 x 784)
-        `y_train`: (10000 x 1)
-    Test data:  
-        `X_test`:  (5000 x 784)
-        `y_test`:  (5000 x 1)
+    Raw:
+        Training data:                
+            `X_train`: (10000 x 784)
+            `y_train`: (10000 x 1)
+        Test data:  
+            `X_test`:  (5000 x 784)
+            `y_test`:  (5000 x 1)
+    Scaled:
+        Training data:                
+            `X_train`: (10000 x 784)
+            `y_train`: (10000 x 1)
+        Test data:  
+            `X_test`:  (5000 x 784)
+            `y_test`:  (5000 x 1)
+    PCA:
+        Training data:                
+            `X_train`: (10000 x 10)
+            `y_train`: (10000 x 1)
+        Test data:  
+            `X_test`:  (5000 x 10)
+            `y_test`:  (5000 x 1)
 
     Returns
     -------
     2d ndarrays
-        numpy data arrays of shape in order of how it is returned:
-            (10000 x 784), (5000 x 784), (10000 x 1) & (5000 x 1)
+        numpy data arrays of shape in order of how it is returned.
     """
     if raw:
         X_train, y_train = np.hsplit(np.load(f"{ROOT_DIR}/data/fashion_train.npy"),[-1])
         X_test, y_test = np.hsplit(np.load(f"{ROOT_DIR}/data/fashion_test.npy"),[-1])
     elif scaled and not raw:
-        X_train, y_train = np.hsplit(np.load(f"{ROOT_DIR}/data/fashion_train.npy"),[-1])
-        X_test, y_test = np.hsplit(np.load(f"{ROOT_DIR}/data/fashion_test.npy"),[-1])
-        SS = StandardScaler().fit(X_train)
-        X_train, X_test = SS.transform(X_train), SS.transform(X_test)
+        X_train, y_train = np.hsplit(np.load(f"{ROOT_DIR}/data/fashion_train_scaled.npy"),[-1])
+        X_test, y_test = np.hsplit(np.load(f"{ROOT_DIR}/data/fashion_test_scaled.npy"),[-1])
+    elif pca and not raw and not scaled:
+        X_train, y_train = np.hsplit(np.load(f"{ROOT_DIR}/data/fashion_train_pca.npy"),[-1])
+        X_test, y_test = np.hsplit(np.load(f"{ROOT_DIR}/data/fashion_test_pca.npy"),[-1])
+        y_train, y_test = y_train.astype(int), y_test.astype(int)
     return X_train, X_test, y_train, y_test
 
 
